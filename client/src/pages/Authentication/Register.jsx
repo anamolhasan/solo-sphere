785a34727c7e096cfe495a42d1f1bc48f24df3a2
anamolhasan <mdanamolhasan1998@gmail.com>
2025-10-18 17:21:14 +0600

    registrion complited

--- client/src/pages/Authentication/Register.jsx
+++ client/src/pages/Authentication/Register.jsx
@@ -11,6 +11,32 @@ const Register = () => {
   const { signInWithGoogle, createUser, updateUserProfile, setUser } =
     useContext(AuthContext)
 
+    const handleSignUp = async e => {
+      e.preventDefault()
+      const form = e.target 
+      const email = form.email.value 
+      const name = form.name.value
+      const photo = form.photo.value 
+      const pass = form.password.value
+      console.log(email, name. photo. pass)
+
+      try {
+        // User Registration
+        const result = await createUser(email, pass)
+        console.log(result)
+        await updateUserProfile(name, photo)
+        setUser({...result.user, photo, displayName: name})
+        toast.success("Signup Successful")
+        navigate('/')
+      } catch (error) {
+        console.log(error)
+        toast.error(error?.message)
+      }
+
+
+
+    }
+
       // Google Signin
     const handleGoogleSignIn = async() => {
         try {
@@ -71,27 +97,63 @@ const Register = () => {
           <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
         </div>
       {/* email and password with register */}
-       <form >
+       <form  onSubmit={handleSignUp}>
+        {/* name */}
           <div className="mt-4">
           <label
             className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-            for="LoggingEmailAddress"
+            htmlFor="name"
+          >
+            Username
+          </label>
+          <input
+            id="name"
+            placeholder='anamol hasan'
+            autoComplete='name'
+            name='name'
+            type="text"
+            className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+          />
+        </div>
+        {/* photo */}
+          <div className="mt-4">
+          <label
+            className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+            htmlFor="photo"
+          >
+            Photo URL
+          </label>
+          <input
+            id="photo"
+            placeholder='enter your photo url'
+            name='photo'
+            type="text"
+            className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+          />
+        </div>
+        {/* email */}
+          <div className="mt-4">
+          <label
+            className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+            htmlFor="LoggingEmailAddress"
           >
             Email Address
           </label>
           <input
             id="LoggingEmailAddress"
             placeholder='anamolhasan.job@gmail.com'
-            className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+            autoComplete='email'
+            name='email'
             type="email"
+            className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
           />
         </div>
-
+        {/* password */}
         <div className="mt-4">
           <div className="flex justify-between">
             <label
               className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-              for="loggingPassword"
+              htmlFor="loggingPassword"
             >
               Password
             </label>
@@ -100,6 +162,7 @@ const Register = () => {
           <input
             id="loggingPassword"
             placeholder='enter your password'
+            name='password'
             className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
             type="password"
           />
