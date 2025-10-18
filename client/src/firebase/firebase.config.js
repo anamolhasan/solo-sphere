c6015f3325f4c0e4e7b36e8c84ccc692dbae7a4f
anamolhasan <mdanamolhasan1998@gmail.com>
2025-10-18 11:18:22 +0600

    auth provider added

--- client/src/firebase/firebase.config.js
+++ client/src/firebase/firebase.config.js
@@ -1,18 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDAuSVctyww_vhkrODANCslzT-gfj5VV7o",
-  authDomain: "solo-shop-ea692.firebaseapp.com",
-  projectId: "solo-shop-ea692",
-  storageBucket: "solo-shop-ea692.firebasestorage.app",
-  messagingSenderId: "130785574925",
-  appId: "1:130785574925:web:db35f5a2d310a288645544"
+  apiKey: "AIzaSyC3Lik_Fm-CP40b9w1LfE5iai-IYCmF82Q",
+  authDomain: "solosphere-ce078.firebaseapp.com",
+  projectId: "solosphere-ce078",
+  storageBucket: "solosphere-ce078.firebasestorage.app",
+  messagingSenderId: "1947377653",
+  appId: "1:1947377653:web:fdd2e546f0b9a668ff0abc"
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
